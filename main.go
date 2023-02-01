2c683ba074b3890db9eb2c17f577f6fb8bef48eb
James Burns <james.burns@rtslabs.com>
2023-02-01 08:48:08 -0500

    put everything in main

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
+	"io"
 	"log"
 	"time"
 
@@ -20,60 +21,7 @@ import (
 //go:embed migrations/*.sql
 var embedMigrations embed.FS
 
-// before running, run `docker-compose -f postgres.yml up`
-func main() {
-	driver := "postgres"
-	connectionString := "user=localuser password=supersecret dbname=testdb sslmode=disable host=localhost"
-	ctx := context.Background()
-
-	migrateWithGoose(driver, connectionString)
-
-	// test methods
-	customSamples := GetAllWithCustom(ctx, driver, connectionString)
-	printSamples("custom", customSamples)
-
-	sqlxSamples := GetAllWithSqlx(ctx, driver, connectionString)
-	printSamples("sqlx", sqlxSamples)
-
-	gormSamples := GetAllWithGorm(connectionString)
-	printSamples("gorm", gormSamples)
-}
-
-func printSamples(source string, samples any) {
-	fmt.Printf("Samples from %s:\n", source)
-	b, _ := json.MarshalIndent(samples, "", "  ")
-	fmt.Println(string(b))
-}
-
-func migrateWithGoose(driverName, dataSourceName string) {
-	d, err := sql.Open(driverName, dataSourceName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer d.Close()
-
-	// try to connect a few times - this is mainly for docker-compose
-	for i := 0; i < 3; i++ {
-		if err = d.Ping(); err == nil {
-			break
-		}
-		log.Println("Waiting one second for db to start...")
-		time.Sleep(time.Second)
-	}
-	if err != nil {
-		_ = d.Close()
-		panic(fmt.Errorf("unable to ping db: %w", err))
-	}
-	log.Println("DB connection successful")
-
-	// do migrations
-	_ = goose.SetDialect("postgres")
-	goose.SetBaseFS(embedMigrations)
-	if err := goose.Up(d, "migrations", goose.WithAllowMissing()); err != nil {
-		panic(err)
-	}
-}
-
+// CustomSample to be used with built-in go sql stuff
 type CustomSample struct {
 	ID          int
 	Name        string
@@ -84,33 +32,7 @@ type CustomSample struct {
 	DeletedAt   *time.Time
 }
 
-func GetAllWithCustom(ctx context.Context, driverName, dataSourceName string) []CustomSample {
-	// connect to db
-	d, err := sql.Open(driverName, dataSourceName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer d.Close()
-
-	// query
-	rows, err := d.QueryContext(ctx, "select * from test.sample_table")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// unmarshal
-	samples := make([]CustomSample, 0)
-	for rows.Next() {
-		var c CustomSample
-		err = rows.Scan(&c.ID, &c.Name, &c.Description, &c.IntExample, &c.CreatedAt, &c.UpdatedAt, &c.DeletedAt)
-		if err != nil {
-			log.Fatal(err)
-		}
-		samples = append(samples, c)
-	}
-	return samples
-}
-
+// SqlxSample to be used with sqlx
 type SqlxSample struct {
 	ID          int        `db:"id"`
 	Name        string     `db:"name"`
@@ -121,21 +43,7 @@ type SqlxSample struct {
 	DeletedAt   *time.Time `db:"deleted_at"`
 }
 
-func GetAllWithSqlx(ctx context.Context, driverName, dataSourceName string) []SqlxSample {
-	db, err := sqlx.ConnectContext(ctx, driverName, dataSourceName)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	samples := make([]SqlxSample, 0)
-	err = db.SelectContext(ctx, &samples, "select * from test.sample_table")
-	if err != nil {
-		panic(err)
-	}
-	return samples
-}
-
-// SampleTable must be named after the table for gorm to find it
+// SampleTable to be used with gorm - must be named after the table
 type SampleTable struct {
 	gorm.Model
 	ID          int
@@ -147,19 +55,132 @@ type SampleTable struct {
 	DeletedAt   *time.Time
 }
 
-func GetAllWithGorm(dataSourceName string) []SampleTable {
-	conn := postgres.Open(dataSourceName)
-	gd, err := gorm.Open(conn, &gorm.Config{
+// before running, run `docker-compose -f postgres.yml up`
+// note, error handling is not done here for ease of comparison
+func main() {
+	driverName := "postgres"
+	connectionString := "user=localuser password=supersecret dbname=testdb sslmode=disable host=localhost"
+	ctx := context.Background() // you don't need to use contexts, but it's good practice
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// make connections
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// custom connection
+	customDBConnection, _ := sql.Open(driverName, connectionString)
+	defer safeClose(customDBConnection)
+
+	// sqlx connection
+	sqlxDBConnection, _ := sqlx.ConnectContext(ctx, driverName, connectionString)
+	defer safeClose(sqlxDBConnection)
+
+	// gorm connection
+	gormDBConnection, _ := gorm.Open(postgres.Open(connectionString), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   "test.",
 			SingularTable: true,
 		},
 	})
-	if err != nil {
-		log.Fatal(fmt.Errorf("creating migrate: %w", err))
-	}
+	defer func() {
+		db, _ := gormDBConnection.DB()
+		_ = db.Close()
+	}()
 
-	samples := make([]SampleTable, 0)
-	_ = gd.Find(&samples)
-	return samples
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// do migrations with goose
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	migrateWithGoose(customDBConnection)
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// test inserts
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// custom insert
+	cs := CustomSample{
+		Name:        "Custom Inserted Sample",
+		Description: ptr("Custom inserted description"),
+	}
+	_, _ = customDBConnection.ExecContext(ctx,
+		"insert into test.sample_table (name, description, int_example) values ($1, $2, $3)",
+		cs.Name, cs.Description, cs.IntExample,
+	)
+
+	// sqlx insert
+	_, _ = sqlxDBConnection.NamedExecContext(ctx,
+		"insert into test.sample_table (name, description) values (:name, :description)",
+		SqlxSample{
+			Name:        "Sqlx Inserted Sample",
+			Description: ptr("Sqlx inserted description"),
+		},
+	)
+
+	// gorm insert
+	_ = gormDBConnection.Create(&SampleTable{
+		Name:        "Gorm Inserted Sample",
+		Description: ptr("Gorm inserted description"),
+	})
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// test selects
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// custom select
+	rows, _ := customDBConnection.QueryContext(ctx, "select * from test.sample_table")
+	customSamples := make([]CustomSample, 0)
+	for rows.Next() {
+		var c CustomSample
+		_ = rows.Scan(&c.ID, &c.Name, &c.Description, &c.IntExample, &c.CreatedAt, &c.UpdatedAt, &c.DeletedAt)
+		customSamples = append(customSamples, c)
+	}
+	printSamples("custom", customSamples)
+
+	// sqlx select
+	sqlxSamples := make([]SqlxSample, 0)
+	_ = sqlxDBConnection.SelectContext(ctx, &sqlxSamples, "select * from test.sample_table")
+	printSamples("sqlx", sqlxSamples)
+
+	// gorm select
+	gormSamples := make([]SampleTable, 0)
+	_ = gormDBConnection.Find(&gormSamples)
+	printSamples("gorm", gormSamples)
+}
+
+func printSamples(source string, samples any) {
+	fmt.Printf("Samples from %s:\n", source)
+	b, _ := json.MarshalIndent(samples, "", "  ")
+	fmt.Println(string(b))
+}
+
+func migrateWithGoose(db *sql.DB) {
+
+	// try to connect a few times - this is mainly for docker-compose
+	var err error
+	for i := 0; i < 3; i++ {
+		if err = db.Ping(); err == nil {
+			break
+		}
+		log.Println("Waiting one second for db to start...")
+		time.Sleep(time.Second)
+	}
+	if err != nil {
+		_ = db.Close()
+		panic(fmt.Errorf("unable to ping db: %w", err))
+	}
+	log.Println("DB connection successful")
+
+	// do migrations
+	_ = goose.SetDialect("postgres")
+	goose.SetBaseFS(embedMigrations)
+	if err := goose.Up(db, "migrations", goose.WithAllowMissing()); err != nil {
+		panic(err)
+	}
+}
+
+// ptr helper function to convert any literal to a pointer
+func ptr[T any](t T) *T {
+	return &t
+}
+
+// safeClose is a helper method that can be used to handle closing errors - or just suppress them
+func safeClose(closer io.Closer) {
+	_ = closer.Close()
 }
